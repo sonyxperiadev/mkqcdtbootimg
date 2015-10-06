3b143bb3b145670a10b733b62e3ff615446a195b
Abhinav Jhanwar <abhinav.jhanwar.august2@gmail.com>
2015-10-06 23:01:07 +0530

    Update mkqcdtbootimg with mkbootimg from marshmallow
    Signed-off-by: Abhinav1997 <abhinav.jhanwar.august2@gmail.com>

--- bootimg.h
+++ bootimg.h
@@ -15,6 +15,8 @@
 ** limitations under the License.
 */
 
+#include <stdint.h>
+
 #ifndef _BOOT_IMAGE_H_
 #define _BOOT_IMAGE_H_
 
@@ -27,30 +29,30 @@ typedef struct boot_img_hdr boot_img_hdr;
 
 struct boot_img_hdr
 {
-    unsigned char magic[BOOT_MAGIC_SIZE];
+    uint8_t magic[BOOT_MAGIC_SIZE];
 
-    unsigned kernel_size;  /* size in bytes */
-    unsigned kernel_addr;  /* physical load addr */
+    uint32_t kernel_size;  /* size in bytes */
+    uint32_t kernel_addr;  /* physical load addr */
 
-    unsigned ramdisk_size; /* size in bytes */
-    unsigned ramdisk_addr; /* physical load addr */
+    uint32_t ramdisk_size; /* size in bytes */
+    uint32_t ramdisk_addr; /* physical load addr */
 
-    unsigned second_size;  /* size in bytes */
-    unsigned second_addr;  /* physical load addr */
+    uint32_t second_size;  /* size in bytes */
+    uint32_t second_addr;  /* physical load addr */
 
-    unsigned tags_addr;    /* physical addr for kernel tags */
-    unsigned page_size;    /* flash page size we assume */
+    uint32_t tags_addr;    /* physical addr for kernel tags */
+    uint32_t page_size;    /* flash page size we assume */
 
-    unsigned dt_size;      /* size in bytes */
+    uint32_t dt_size;      /* size in bytes */
 
-    unsigned unused[1];    /* future expansion: should be 0 */
+    uint32_t unused[1];    /* future expansion: should be 0 */
 
-    unsigned char name[BOOT_NAME_SIZE]; /* asciiz product name */
+    uint8_t name[BOOT_NAME_SIZE]; /* asciiz product name */
     
-    unsigned char cmdline[BOOT_ARGS_SIZE];
+    uint8_t cmdline[BOOT_ARGS_SIZE];
 
-    unsigned id[8]; /* timestamp / checksum / sha1 / etc */
-};
+    uint32_t id[8]; /* timestamp / checksum / sha1 / etc */
+} __attribute__((packed));
 
 /*
 ** +-----------------+ 
